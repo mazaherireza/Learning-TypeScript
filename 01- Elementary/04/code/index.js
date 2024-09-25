79500c51b04443e5e16a1a8874d5f0d57ecb2b0a
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-25 13:06:37 +0330

    Elementary: 04

---
+++ 01- Elementary/04/code/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const person = {
+    firstName: "Reza",
+    lastName: "Mazaheri",
+    age: 35,
+    married: false,
+};
+console.log(`${person.firstName} ${person.lastName}`);
---
+++ 01- Elementary/04/code/index.ts
@@ -0,0 +1,13 @@
+const person: {
+  firstName: string;
+  lastName: string;
+  age: number;
+  married: boolean;
+} = {
+  firstName: "Reza",
+  lastName: "Mazaheri",
+  age: 35,
+  married: false,
+};
+
+console.log(`${person.firstName} ${person.lastName}`);
