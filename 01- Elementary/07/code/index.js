14d9c261c0ae8899e02570d56991066784113bc1
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-27 12:18:52 +0330

    Elementary: 07

---
+++ 01- Elementary/07/code/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let everything; // any
+everything = "Reza";
+everything = 35;
+everything = false;
---
+++ 01- Elementary/07/code/index.ts
@@ -0,0 +1,4 @@
+let everything  // any
+everything = "Reza"
+everything = 35
+everything = false
