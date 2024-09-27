7c758459dd81804149e0a881524ef680bf240ba7
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-27 13:02:38 +0330

    Elementary: 09

---
+++ 01- Elementary/09/code/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let x;
+x = null;
+let y;
+//y = 1001;
+y = undefined;
+const prenom = "Reza";
+console.log(prenom);
---
+++ 01- Elementary/09/code/index.ts
@@ -0,0 +1,10 @@
+let x: null;
+x = null;
+
+let y: undefined;
+//y = 1001;
+y = undefined;
+
+const prenom: "Reza" = "Reza";
+
+console.log(prenom);
