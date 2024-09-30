3d3b6f9a3e9477c7c8998c28b277c3533f8b5a26
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-30 09:50:58 +0330

    Elementary: 23

---
+++ 01- Elementary/23/code/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const books = ["Animal Farm", "How to Stop Wrorrying and Start Living"];
+console.log(books);
---
+++ 01- Elementary/23/code/index.ts
@@ -0,0 +1,6 @@
+const books: Array<string> = [
+  "Blindness",
+  "How to Stop Wrorrying and Start Living",
+];
+
+console.log(books);
