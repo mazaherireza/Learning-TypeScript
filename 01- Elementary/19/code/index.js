80b18c5b6043b4919fdfb52a60d88cbbccdc269e
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-30 09:49:06 +0330

    Elementary: 19

---
+++ 01- Elementary/19/code/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// Abstract methods are methods that we MUST define on any implementing subclass.
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+class Ebook extends Book {
+    constructor(title, author) {
+        super(title, author);
+    }
+    getBookType() {
+        return "Ebook";
+    }
+}
+const book = new Ebook("1984", "George Orwell");
+console.log(book.getBookType());
---
+++ 01- Elementary/19/code/index.ts
@@ -0,0 +1,23 @@
+// Abstract methods are methods that we MUST define on any implementing subclass.
+
+abstract class Book {
+  title: string;
+  author: string;
+  constructor(title: string, author: string) {
+    this.title = title;
+    this.author = author;
+  }
+  abstract getBookType(): string; // There is no implementation for the abstract method
+}
+
+class Ebook extends Book {
+  constructor(title: string, author: string) {
+    super(title, author);
+  }
+  getBookType(): string {
+    return "Ebook";
+  }
+}
+
+const book = new Ebook("The Visit", "Friedrich Dürrenmatt");
+console.log(book.getBookType());
