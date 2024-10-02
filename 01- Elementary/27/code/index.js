9a1c239861b75df08eb0e62b445c7e178a4014a7
mazaherireza <rezamazaheri.email@gmail.com>
2024-10-02 09:02:56 +0330

    Elementary: 27

---
+++ 01- Elementary/27/code/index.js
@@ -0,0 +1,19 @@
+"use strict";
+class Elements {
+    constructor() {
+        this.list = [];
+    }
+    addElement(element) {
+        this.list.push(element);
+    }
+    removeElement(element) {
+        const index = this.list.indexOf(element);
+        this.list.splice(index, 1);
+    }
+    printElement(element) {
+        console.log(element.username);
+    }
+}
+const element = new Elements();
+element.addElement({ id: "88008800", username: "rezamazaheri" });
+element.printElement({ id: "88008800", username: "rezamazaheri" });
---
+++ 01- Elementary/27/code/index.ts
@@ -0,0 +1,25 @@
+interface User {
+  id: string;
+  username: string;
+}
+
+class Elements<T extends User> {
+  list: T[] = [];
+
+  addElement(element: T) {
+    this.list.push(element);
+  }
+
+  removeElement(element: T) {
+    const index = this.list.indexOf(element);
+    this.list.splice(index, 1);
+  }
+
+  printElement(element: T) {
+    console.log(element.username);
+  }
+}
+
+const element = new Elements();
+element.addElement({ id: "88008800", username: "rezamazaheri" });
+element.printElement({ id: "88008800", username: "rezamazaheri" })
