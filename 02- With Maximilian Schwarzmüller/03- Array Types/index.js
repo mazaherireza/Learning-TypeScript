836d731b445395fb9ee303b56841f38a55147b09
mazaherireza <rezamazaheri.email@gmail.com>
2025-02-19 16:37:17 +0330

    03- Array Types

---
+++ 02- With Maximilian Schwarzmüller/03- Array Types/index.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "Reza",
+    age: 36,
+    hobbies: ["Reading Books", "Learning", "Cooking"],
+};
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toLowerCase());
+}
---
+++ 02- With Maximilian Schwarzmüller/03- Array Types/index.ts
@@ -0,0 +1,7 @@
+const person = {
+  name: "Reza",
+  age: 36,
+  hobbies: ["Reading Books", "Learning", "Cooking"],
+};
+
+for (const hobby of person.hobbies) console.log(hobby.toLowerCase());
