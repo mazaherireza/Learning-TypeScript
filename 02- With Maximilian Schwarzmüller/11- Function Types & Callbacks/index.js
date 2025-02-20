bf68fda558d52b53243591248fab0520f88a9aef
mazaherireza <rezamazaheri.email@gmail.com>
2025-02-20 13:52:33 +0330

    03- Function Types & Callbacks - Edition

--- 02- With Maximilian Schwarzmüller/11- Function Types & Callbacks/index.js
+++ 02- With Maximilian Schwarzmüller/11- Function Types & Callbacks/index.js
@@ -1,3 +1,10 @@
-var add = function (num1, num2) {
-    return num1 + num2;
+var func = function (a, b, callback) {
+    var result = a + b;
+    callback(result);
+    // void <------- We'll ignore any result you might be returning here.
 };
+func(1001, 1002, function (input) {
+    // console.log(input)
+    return input;
+    // The only thing TS does not pick up is, if we return something here.
+});
