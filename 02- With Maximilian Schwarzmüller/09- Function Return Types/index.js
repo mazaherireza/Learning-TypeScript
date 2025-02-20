4e72cdf3fdee7aa478e449e6adbc1d78a2eb6689
mazaherireza <rezamazaheri.email@gmail.com>
2025-02-20 12:54:47 +0330

    03- Function Return Types

---
+++ 02- With Maximilian Schwarzmüller/09- Function Return Types/index.js
@@ -0,0 +1,6 @@
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var display = function (input) {
+    console.log(input);
+};
---
+++ 02- With Maximilian Schwarzmüller/09- Function Return Types/index.ts
@@ -0,0 +1,7 @@
+const add = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+const display = (input: number): void => {
+  console.log(input);
+};
