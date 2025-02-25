7a0a519962e136c5ba7ba7d80f95623d57430597
mazaherireza <rezamazaheri.email@gmail.com>
2025-02-25 21:50:37 +0330

    15- Classes & Interfaces - Part 3

---
+++ 02- With Maximilian Schwarzmüller/15- Classes & Interfaces/03- Part Three/index.ts
@@ -0,0 +1,19 @@
+class Department {
+  title: string;
+  private employees: string[] = [];
+
+  constructor(_title: string) {
+    this.title = _title;
+  }
+  describe(this: Department) {
+    console.log(`Department: ${this.title}`);
+  }
+  addEmployee(employee: string) {
+    // ... one uniform of maniuplating employees. ... doing some verification or ...
+    this.employees.push(employee);
+  }
+}
+
+const hardware_network = new Department("Hardware & Network");
+hardware_network.describe();
+console.log(hardware_network.addEmployee("Milad Mohadmmadi"));
