80e8f7a961463365487bc44973a42a13c04f753f
mazaherireza <rezamazaheri.email@gmail.com>
2025-02-26 10:51:56 +0330

    15- Classes & Interfaces - Part 4

---
+++ 02- With Maximilian Schwarzmüller/15- Classes & Interfaces/04- Part Four/index.ts
@@ -0,0 +1,24 @@
+class Department {
+  private employees: string[] = [];
+  /*
+   TS provides 3 access modifiers to class properties and methods:
+   private, protected, and public ... 
+  */
+  constructor(private _id: string, public title: string) {}
+
+  describe(this: Department) {
+    console.log(`Department: ${this.title} with id: ${this._id}`);
+  }
+  addEmployee(employee: string) {
+    // ... one uniform of maniuplating employees. ... doing some verification or ...
+    this.employees.push(employee);
+  }
+  logEmployees() {
+    console.log(this.employees);
+  }
+}
+
+const hardware_network = new Department("DEP_8008", "Hardware & Network");
+hardware_network.describe();
+console.log(hardware_network.addEmployee("Milad Mohammadi"));
+hardware_network.logEmployees();
