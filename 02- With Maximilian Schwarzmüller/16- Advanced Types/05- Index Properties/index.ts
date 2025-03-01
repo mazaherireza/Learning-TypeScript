c1a24a64f972a76b6db8f37606e8e2f871333756
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-01 14:00:57 +0330

    Advanced Types - 05- Index Properties

---
+++ 02- With Maximilian Schwarzmüller/16- Advanced Types/05- Index Properties/index.ts
@@ -0,0 +1,10 @@
+interface _Error {
+  id: string;
+  [property: string]: string; // I don't know the exact property name, I also don't know the property count.
+}
+
+const error: _Error = {
+  id: "ER_800800",
+  email: "Erro 80",
+  username: "Must start with a character.",
+};
