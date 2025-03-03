0e32f179ffb4c346805d6b510ca9056e4ed58e98
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-03 11:45:55 +0330

    07- Decoratores 01- First Class Decorator

---
+++ 02- With Maximilian Schwarzmüller/07- Decorators/01- First Class Decorator/index.ts
@@ -0,0 +1,26 @@
+/*
+  tsconfig.json
+  ...
+  "experimentalDecorators": true,
+*/
+
+// Decorators in general are all about classes.
+// ... how many arguments depends on where you use the decorator.
+function Logger(constructor: Function) {
+  console.log("Logger ...");
+  console.log(constructor);
+}
+
+@Logger // <---------- Decorator
+class Person {
+  firstName = "Reza";
+  constructor() {
+    console.log("Constructor is called.");
+  }
+}
+
+const me = new Person();
+
+/*
+  Decorators execute when your class is DEFINED, Not when it is instantiated.
+*/
