c20d4c31d033de4432d02c8fafd231e58bb8ab7f
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-04 10:47:21 +0330

    07- Decoratores 07- When Do Decorators Execute

---
+++ 02- With Maximilian Schwarzmüller/07- Decorators/07- When Do Decorators Execute/index.ts
@@ -0,0 +1,10 @@
+/* 
+  ... the order in which decorators run though.
+  ... they're all running without instantiating.
+    
+  All decorators no matter if it was a class decorator, method decorator or ... 
+  they all execute when you define class. <------------ **
+
+  ... functions that executes when your class is defined.
+  ... use decorator to do some behind the scenes work 
+*/
