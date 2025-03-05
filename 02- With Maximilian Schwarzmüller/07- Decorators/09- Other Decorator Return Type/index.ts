7c8eddc9c205f9a6643b4cf6b5cb66228ff20cbd
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-05 19:54:56 +0330

    07- Decorators 09- Other Decorator Return Type

---
+++ 02- With Maximilian Schwarzmüller/07- Decorators/09- Other Decorator Return Type/index.ts
@@ -0,0 +1,7 @@
+/*
+  Decorators where you can return something are the decorators you add to
+  methods ... add to accessors.
+  The decorators on properties and on prameters can return something but TS will ignore it.
+
+  If the property descriptor is brand-new to you, ... 
+*/
