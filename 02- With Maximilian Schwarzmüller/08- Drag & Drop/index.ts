755baf9e83914d8bb3b1b4e99430e13acec126de
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-08 10:01:37 +0330

    08- Drag & Drop - Fetching User Inputs

--- 02- With Maximilian Schwarzmüller/08- Drag & Drop/index.ts
+++ 02- With Maximilian Schwarzmüller/08- Drag & Drop/index.ts
@@ -18,8 +18,9 @@ const AutoBind = function (
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
   element: HTMLFormElement;
-  titleInputElement: HTMLInputElement;
+  titleElement: HTMLInputElement;
   descriptionElement: HTMLInputElement;
 
   constructor() {
@@ -36,7 +37,7 @@ class ProjectInput {
       document.getElementById() as HTMLTemplateElement
     */
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
+    this.formElement = document.querySelector("form")! as HTMLFormElement;
     /*
       When we create a new instance of this class, I immediately want to render a form 
       that belongs to this instance.
@@ -47,7 +48,7 @@ class ProjectInput {
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
-    this.titleInputElement = this.element.querySelector(
+    this.titleElement = this.element.querySelector(
       "#title"
     ) as HTMLInputElement;
 
@@ -58,6 +59,16 @@ class ProjectInput {
     this.attach();
   }
 
+  private clearForm() {
+    this.formElement.reset();
+  }
+
+  private gatherUserInput(): [string, string] {
+    const title = this.titleElement.value;
+    const description = this.descriptionElement.value;
+    return [title, description];
+  }
+
   private submitHandler(event: Event) {
     event.preventDefault();
     // this <------ not point to the class actually.
@@ -65,7 +76,12 @@ class ProjectInput {
       When we bind something to an event ... the method ... will have this bound to something else.
       ... current target of the event.
     */
-    console.log(this.titleInputElement.value);
+    const input = this.gatherUserInput();
+    if (Array.isArray(input)) {
+      const [title, description] = input;
+    }
+    console.log(this.titleElement.value);
+    this.clearForm();
   }
 
   @AutoBind
@@ -80,3 +96,18 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+
+/*
+  You shouldn't use undefind as a return type on functions, instead use void.
+  f(): [string] | void {
+    ...
+    if() return 
+    else {}
+  }
+*/
+
+/*
+  ... to check whether it is a tuple ... tuples in the end are just arrays
+*/
+
+// Reusabel scalable way
