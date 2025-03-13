f3f87662cd726b2e88bc746cd43493e09cc71d8e
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-13 06:44:28 +0330

    11- Third Party Libraries & TS 02- Using JS Libraries With TS

---
+++ 02- With Maximilian Schwarzmüller/11- Third Party Libraries & TS/02- Using JS Libraries With TS/code/src/utils/validation.ts
@@ -0,0 +1,16 @@
+import { Validatable } from "../models/validatable";
+
+export function validate(input: Validatable) {
+  let isValid = true;
+  if (input.required)
+    isValid = isValid && input.value.toString().trim().length !== 0;
+  if (input.minLength != null && typeof input.value == "string")
+    isValid = isValid && input.value.length >= input.minLength;
+  if (input.maxLength != null && typeof input.value == "string")
+    isValid = isValid && input.value.length <= input.maxLength;
+  if (input.min != null && typeof input.value == "number")
+    isValid = isValid && input.value >= input.min;
+  if (input.max != null && typeof input.value == "number")
+    isValid = isValid && input.value <= input.max;
+  return isValid;
+}
