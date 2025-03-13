1003041bae3539c1d371abaccfec1231a2d19baf
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-13 14:39:21 +0330

    11- Third Party Libraries & TS 05- Edition

--- 02- With Maximilian Schwarzmüller/11- Third Party Libraries & TS/05- class-validator/code/webpack.config.prod.js
+++ 02- With Maximilian Schwarzmüller/11- Third Party Libraries & TS/05- class-validator/code/webpack.config.prod.js
@@ -3,7 +3,7 @@ const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.ts",
+  entry: "./index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
