2af5b56416db44f91122221419c68fa4ba052fa0
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-25 13:08:30 +0330

    Elementary: 25- Edition

--- 01- Elementary/25/code/index.js
+++ 01- Elementary/25/code/index.js
@@ -1,5 +1,5 @@
 "use strict";
 function information(title, pages) {
-    console.log(`${title} has ${pages} pages`);
+  console.log(`${title} has ${pages} pages`);
 }
 information("A Man Called Ove", 425);
