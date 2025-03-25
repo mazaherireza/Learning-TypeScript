b10a3b954813cfbe785b478142858d9bba178541
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-25 16:45:48 +0330

    Elementary: 31- Edition

--- 01- Elementary/31/code/index.js
+++ 01- Elementary/31/code/index.js
@@ -1,33 +1,44 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+var __decorate =
+  (this && this.__decorate) ||
+  function (decorators, target, key, desc) {
+    var c = arguments.length,
+      r =
+        c < 3
+          ? target
+          : desc === null
+          ? (desc = Object.getOwnPropertyDescriptor(target, key))
+          : desc,
+      d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function")
+      r = Reflect.decorate(decorators, target, key, desc);
+    else
+      for (var i = decorators.length - 1; i >= 0; i--)
+        if ((d = decorators[i]))
+          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
+  };
 const prevention = () => {
-    return function (target, propertyKey, descriptor) {
-        console.log(target);
-        console.log(propertyKey);
-        console.log(descriptor);
-    };
+  return function (target, propertyKey, descriptor) {
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+  };
 };
 class Project {
-    constructor(title, deadline, collaborators) {
-        console.log("___________Constructor___________");
-        this.title = title;
-        this.deadline = deadline;
-        this.collaborators = collaborators;
-    }
-    getTitle() {
-        return this.title;
-    }
+  constructor(title, deadline, collaborators) {
+    console.log("___________Constructor___________");
+    this.title = title;
+    this.deadline = deadline;
+    this.collaborators = collaborators;
+  }
+  getTitle() {
+    return this.title;
+  }
 }
-__decorate([
-    prevention()
-], Project.prototype, "getTitle", null);
+__decorate([prevention()], Project.prototype, "getTitle", null);
 const project = new Project("Online Shop", "30 May", [
-    "Reza Mazaheri",
-    "Yousef Amini",
+  "Reza Mazaheri",
+  "Yousef Amini",
 ]);
 console.log(project);
