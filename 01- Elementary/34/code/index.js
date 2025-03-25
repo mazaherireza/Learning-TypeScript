1a6cc312894b2924bccb38acec4c91ab53e6d470
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-25 17:49:26 +0330

    Elementary: 34- Edition

--- 01- Elementary/34/code/index.js
+++ 01- Elementary/34/code/index.js
@@ -1,6 +1,14 @@
-define(["require", "exports", "./teacher"], function (require, exports, teacher_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const teacher = new teacher_1.Teacher("Hassan", "Khotanlou", "Computer Engineering");
-    console.log(teacher.getFullname());
+define(["require", "exports", "./teacher"], function (
+  require,
+  exports,
+  teacher_1
+) {
+  "use strict";
+  Object.defineProperty(exports, "__esModule", { value: true });
+  const teacher = new teacher_1.Teacher(
+    "Hassan",
+    "Khotanlou",
+    "Computer Engineering"
+  );
+  console.log(teacher.getFullname());
 });
