df07654475d731cde34e5fb319fcfc333355814f
mazaherireza <rezamazaheri.email@gmail.com>
2025-03-25 16:36:29 +0330

    Elementary: 28- Edition

--- 01- Elementary/28/code/index.js
+++ 01- Elementary/28/code/index.js
@@ -1,17 +1,17 @@
 "use strict";
 const exam = {
-    title: "Geometry",
-    deadline: "85",
+  title: "Geometry",
+  deadline: "85",
 };
 console.log(exam);
 var Role;
 (function (Role) {
-    Role["Admin"] = "Admin";
-    Role["user"] = "User";
+  Role["Admin"] = "Admin";
+  Role["user"] = "User";
 })(Role || (Role = {}));
 const user = {
-    firstName: "Reza",
-    lastName: "Mazaheri",
-    role: Role.Admin,
+  firstName: "Reza",
+  lastName: "Mazaheri",
+  role: Role.Admin,
 };
 console.log(user);
